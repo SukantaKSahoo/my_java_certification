c27cb22215923bd310e4011e3999e5c5538ddc2d
Sukanta <mesukanta2017@gmail.com>
2022-10-11 16:51:56 +0530

    Added WrapperDemo file for Wrapper Class Demo

---
+++ src/main/java/com/softycon/java_libraries/java/lang/wrapper_class/WrapperDemo.java
@@ -0,0 +1,10 @@
+package com.softycon.java_libraries.java.lang.wrapper_class;
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        int primitiveInt = 0;
+        Integer wrapperInt = null;
+        System.out.println("Value of Primitive int is " + primitiveInt);
+        System.out.println("Value of Wrapper Integer is " + wrapperInt);
+    }
+}
